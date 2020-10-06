ce5d0a9449bceaadf509601982b93d8c8f7cb8e4
JonathanRubio <jonathanadonayrj@ufps.edu.co>
2020-10-06 14:25:57 -0500

    botones y enlaces a paginas externas (quizas penultima actualizacion

--- 1er Parcial 1151521/JavaScript/JavaScript.js
+++ 1er Parcial 1151521/JavaScript/JavaScript.js
@@ -1,3 +1,7 @@
 $("#que").click(function (event) {
     $("#contenedor").load('Complementos/compl1.html');
   });
+
+$("#sin").click(function (event) {
+    $("#con").load('Complementos/compl2.html');
+});
